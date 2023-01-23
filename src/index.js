72a0432377518e23713b1604b5c6318ad213e633
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-23 17:46:49 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './css/styles.css';
+// all modules
+import Notiflix from 'notiflix';
+
+// one by one
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Report } from 'notiflix/build/notiflix-report-aio';
+// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
+// import { Loading } from 'notiflix/build/notiflix-loading-aio';
+// import { Block } from 'notiflix/build/notiflix-block-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+
+//https://restcountries.com/v3.1/name/united обьекты стран
+//https://restcountries.com/v3.1/name/{name} 404 not found
+ fetch (`https://restcountries.com/v3.1/name/united`);
